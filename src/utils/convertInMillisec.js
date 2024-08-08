64f93fb86dca8bcc61c7189d4cc4adbd174c36c1
Anastasia Zatyamina <anastasia.zatyamina@yandex.com>
2024-08-08 16:04:53 +0300

    timer

---
+++ src/utils/convertInMillisec.js
@@ -0,0 +1,7 @@
+const convertInMillisec = (min, sec) => {
+  const convertMinutes = Number(min) * 60 * 1000
+  const convertSeconds = Number(sec) * 1000
+  return convertMinutes + convertSeconds
+}
+
+export default convertInMillisec
